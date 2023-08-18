30b2849dc863c9085ddc74ee5d416d8ff769b637
Preeyanuch4748 <preeyanuchhomseang@gmail.com>
2023-08-18 08:23:48 +0700

    web.me

---
+++ web/Web.js
@@ -0,0 +1,13 @@
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+      // ...
+    },
+    options: {
+      responsive: true, // ทำให้แผนภูมิเรียงตามขนาดหน้าจอ
+      maintainAspectRatio: false, // ไม่รักษาอัตราส่วนเดิม
+      aspectRatio: 0.8, // อัตราส่วนความกว้างต่อความสูงของแผนภูมิ
+      // ...
+    }
+  });
+  
